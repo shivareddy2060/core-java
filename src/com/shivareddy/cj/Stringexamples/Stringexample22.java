f0a89317dafcdf2ace1b74a4fd12a772616ff4de
shivareddy2060 <shivareddy20051995@gmail.com>
2021-08-05 16:10:40 +0530

    written logic for String length

---
+++ src/com/shivareddy/cj/Stringexamples/Stringexample22.java
@@ -0,0 +1,12 @@
+package com.shivareddy.cj.Stringexamples;
+
+public class Stringexample22 {
+
+	public static void main(String[] args) {
+		String str =" nasarreddy ";
+		
+		System.out.println(str.length());
+		System.out.println(str.trim().length());
+	}
+
+}
